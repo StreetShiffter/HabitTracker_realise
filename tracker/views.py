2298bb704162d4c95061f164bdd797872af4fc09
StreetShiffter <agwzdushaz@gmail.com>
2025-10-13 19:33:50 +0700

    Add viewset app tracker + urls

--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from rest_framework.pagination import PageNumberPagination
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Habit
+from .serializers import HabitSerializer
+from .permissions import IsOwnerOrReadOnlyForPublic
 
-# Create your views here.
+
+class HabitPagination(PageNumberPagination):
+    """Пагинация для вывода списка привычек на странице"""
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 50
+
+
+class HabitViewSet(viewsets.ModelViewSet):
+    """Вьюсет для модели привычки с разделением прав и сереализатором"""
+    serializer_class = HabitSerializer
+    pagination_class = HabitPagination
+    permission_classes = [IsOwnerOrReadOnlyForPublic]
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ['is_public']
+    ordering_fields = ['time', 'periodicity']
+    ordering = ['time']
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return Habit.objects.all()
+
+        # Для списка: свои + публичные
+        if self.action == 'list':
+            return Habit.objects.filter(
+                owner=user
+            ) | Habit.objects.filter(is_public=True)
+
+        # Для retrieve/update/delete: только свои или публичные (но редактировать нельзя)
+        return Habit.objects.filter(owner=user) | Habit.objects.filter(is_public=True)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
