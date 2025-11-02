33699c41e25d28515d1fea03002950cb514413f7
StreetShiffter <agwzdushaz@gmail.com>
2025-11-02 21:21:54 +0700

    deploy_dockerhub4

--- users/tests.py
+++ users/tests.py
@@ -6,7 +6,6 @@ from rest_framework import status
 from users.models import User
 from unittest.mock import patch
 from users.permissions import IsOwnerOrAdminForProfile
-from users.services import send_telegram_message
 
 
 class UserModelTest(TestCase):
@@ -259,6 +258,8 @@ class UserServiceTest(TestCase):
         mock_send_telegram.return_value = {"ok": True}
         # До патча функция вызывалась оригинально, но после патча вызывая оригинал
         # подставляется патч и работает мок объект
+        from users.services import send_telegram_message
+
         result = send_telegram_message("123456789", "Test message")
 
         # Проверяем, что мок был вызван с правильными аргументами
